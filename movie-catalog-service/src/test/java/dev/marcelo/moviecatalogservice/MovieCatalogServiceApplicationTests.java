16be29e2f689de9fc883d6bb24f1c2bc42765555
Marcelo Abreu <marceloabr.work@gmail.com>
2024-01-04 13:50:17 +0000

    init

---
+++ movie-catalog-service/src/test/java/dev/marcelo/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcelo.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
